cd033154f02989289e163f55e2d5c74641b3d667
EdwardPhaniOS <EdwardPhan2015@gmail.com>
2019-08-05 20:27:52 +0700

    Them chart

--- FlashCard/app/src/main/java/com/example/flashcard/UserProgress.java
+++ FlashCard/app/src/main/java/com/example/flashcard/UserProgress.java
@@ -1,14 +1,12 @@
 package com.example.flashcard;
 
 import android.os.Bundle;
-
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-
 import android.view.MenuItem;
+
 
 public class UserProgress extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
@@ -23,6 +21,7 @@ public class UserProgress extends AppCompatActivity implements BottomNavigationV
         //getting bottom navigation view and attaching the listener
         BottomNavigationView navigation = findViewById(R.id.nav_view);
         navigation.setOnNavigationItemSelectedListener(this);
+
     }
 
     @Override
--- FlashCard/app/src/main/java/com/example/flashcard/UserProgressFragment.java
+++ FlashCard/app/src/main/java/com/example/flashcard/UserProgressFragment.java
@@ -11,16 +11,49 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.anychart.AnyChart;
+import com.anychart.AnyChartView;
+import com.anychart.chart.common.dataentry.DataEntry;
+import com.anychart.chart.common.dataentry.ValueDataEntry;
+import com.anychart.charts.Pie;
+import com.anychart.enums.Align;
+import com.anychart.enums.LegendLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserProgressFragment extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        //just change the fragment_dashboard
-        //with the fragment you want to inflate
-        //like if the class is HomeFragment it should have R.layout.home_fragment
-        //if it is DashboardFragment it should have R.layout.fragment_dashboard
-        return inflater.inflate(R.layout.fragment_user_progress, null);
+        View rootView = inflater.inflate(R.layout.fragment_user_progress, null);
+
+        //adding chart
+        Pie pie = AnyChart.pie();
+
+        List<DataEntry> data = new ArrayList<>();
+        data.add(new ValueDataEntry("Thuộc lòng", 10000));
+        data.add(new ValueDataEntry("Sơ sơ", 12000));
+        data.add(new ValueDataEntry("Đã quên", 18000));
+        data.add(new ValueDataEntry("Chưa thuộc", 18000));
+
+        pie.data(data);
+
+        AnyChartView anyChartView = (AnyChartView) rootView.findViewById(R.id.any_chart_view);
+
+        pie.title("Progression Report");
+
+        pie.labels().position("outside");
+
+        pie.legend()
+                .position("center-bottom")
+                .itemsLayout(LegendLayout.HORIZONTAL)
+                .align(Align.CENTER);
+
+        anyChartView.setChart(pie);
+
+        return rootView;
     }
 
 
